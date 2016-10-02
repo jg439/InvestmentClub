8451a065889a0058f0ebb3234b9414cdbbc31695
Julia Garcia <jg439@njit.edu>
2016-10-02 19:38:09 -0400

    file

---
+++ media.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+	<html lang = "en">
+		<head>
+
+			<title>NJIT Investment Club </title>
+				<meta charset = "utf-8">
+				<meta name = "description" content="Student-managed club where you can find information about investing, our events and portfolio">
+				<meta name = "keywords" content ="NJIT, investing,investment,students, portfolio">
+
+				<link rel = "shortcut icon" href ="images/favicon.ico">
+				<link rel = "stylesheet" type ="text/css" href = "styles/media.css">
+				<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+				<script type="text/javascript" src="js/jssor.slider-21.1.5.mini.js"></script>
+		</head>
+
+		<body>
+			<header>
+				<nav id = "upper_nav">
+					<ul>
+						<li><a href = "#log">Login</a></li>
+						<li><a href = "registration.html">Register</a></li>
+						<li><a href = "">Search</a></li>
+					</ul>
+				</nav>
+
+
+				<img src = "images/NJIT_logo.png" alt = "logo" width ="250" height= "100">
+
+				<div id="header">
+					<ul class="nav">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="">About</a>
+							<ul>
+								<li><a href="team.php">Team</a></li>
+								<li><a href="challenges.php">Challenges</a></li>
+							</ul>
+						</li>
+							
+						<li><a href="">Portfolio</a>
+							<ul>
+								<li><a href="investmentphilosophy.php">Our Investment Philosophy</a></li>
+								<li><a href="executedtrades.php">Executed Trades</a></li>
+							</ul>
+						</li>
+							
+						<li><a href="calendar.php">Calendar</a></li>
+						<li><a href="">Classes</a>
+							<ul>
+								<li><a href="investing101.php">Investing 101</a></li>
+								<li><a href="bloomberg.php">Bloomberg Terminal</a></li>
+							</ul>
+						</li>
+						<li><a href="media.php">Media</a></li>
+						<li><a href="faq.php">FAQ</a></li>
+						<li><a href="contactus.php">Contact Us</a></li>
+			
+					</ul>
+				</div>	
+
+			</header>
+
+			<main style="padding:0px; margin:0px; background-color:#fff;font-family:Arial, sans-serif">	
+
+				<h1> Media </h1>
+
+			<div id="jssor_1" style="position: relative; margin: 0 auto; top: 0px; left: 0px; width: 960px; height: 480px; overflow: hidden; visibility: hidden; background-color: #24262e;">
+			<!-- Loading Screen -->
+        <div data-u="loading" style="position: absolute; top: 0px; left: 0px;">
+            <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block; top: 0px; left: 0px; width: 100%; height: 100%;"></div>
+            <div style="position:absolute;display:block;background:url('img/loading.gif') no-repeat center center;top:0px;left:0px;width:100%;height:100%;"></div>
+        </div>
+        <div data-u="slides" style="cursor: default; position: relative; top: 0px; left: 240px; width: 720px; height: 480px; overflow: hidden;">
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+            <div data-p="150.00" style="display: none;">
+                <img data-u="image" src="images/bloombergpic.jpg" />
+                <img data-u="thumb" src="images/bloombergpic.jpg" />
+            </div>
+        
+        </div>
+        <!-- Thumbnail Navigator -->
+        <div data-u="thumbnavigator" class="jssort01-99-66" style="position:absolute;left:0px;top:0px;width:240px;height:480px;" data-autocenter="2">
+            <!-- Thumbnail Item Skin Begin -->
+            <div data-u="slides" style="cursor: default;">
+                <div data-u="prototype" class="p">
+                    <div class="w">
+                        <div data-u="thumbnailtemplate" class="t"></div>
+                    </div>
+                    <div class="c"></div>
+                </div>
+            </div>
+            <!-- Thumbnail Item Skin End -->
+        </div>
+        <!-- Arrow Navigator -->
+        <span data-u="arrowleft" class="jssora05l" style="top:158px;left:248px;width:40px;height:40px;" data-autocenter="2"></span>
+        <span data-u="arrowright" class="jssora05r" style="top:158px;right:8px;width:40px;height:40px;" data-autocenter="2"></span>
+    </div>
+
+    <!-- #endregion Jssor Slider End -->	
+			
+			
+			
+			
+			<main>
+
+
+		</body>
+
+
+	</html>
+	
+	<script type="text/javaScript">
+				jQuery(document).ready(function ($) {
+            
+            var jssor_1_SlideshowTransitions = [
+              {$Duration:1200,$Zoom:1,$Easing:{$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad},$Opacity:2},
+              {$Duration:1000,$Zoom:11,$SlideOut:true,$Easing:{$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:1200,$Zoom:1,$Rotate:1,$During:{$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
+              {$Duration:1000,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
+              {$Duration:1200,x:0.5,$Cols:2,$Zoom:1,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:1200,x:4,$Cols:2,$Zoom:11,$SlideOut:true,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear},$Opacity:2},
+              {$Duration:1200,x:0.6,$Zoom:1,$Rotate:1,$During:{$Left:[0.2,0.8],$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Left:$Jease$.$Swing,$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
+              {$Duration:1000,x:-4,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
+              {$Duration:1200,x:-0.6,$Zoom:1,$Rotate:1,$During:{$Left:[0.2,0.8],$Zoom:[0.2,0.8],$Rotate:[0.2,0.8]},$Easing:{$Left:$Jease$.$Swing,$Zoom:$Jease$.$Swing,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$Swing},$Opacity:2,$Round:{$Rotate:0.5}},
+              {$Duration:1000,x:4,$Zoom:11,$Rotate:1,$SlideOut:true,$Easing:{$Left:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.8}},
+              {$Duration:1200,x:0.5,y:0.3,$Cols:2,$Zoom:1,$Rotate:1,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.7}},
+              {$Duration:1000,x:0.5,y:0.3,$Cols:2,$Zoom:1,$Rotate:1,$SlideOut:true,$Assembly:2049,$ChessMode:{$Column:15},$Easing:{$Left:$Jease$.$InExpo,$Top:$Jease$.$InExpo,$Zoom:$Jease$.$InExpo,$Opacity:$Jease$.$Linear,$Rotate:$Jease$.$InExpo},$Opacity:2,$Round:{$Rotate:0.7}},
+              {$Duration:1200,x:-4,y:2,$Rows:2,$Zoom:11,$Rotate:1,$Assembly:2049,$ChessMode:{$Row:28},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.7}},
+              {$Duration:1200,x:1,y:2,$Cols:2,$Zoom:11,$Rotate:1,$Assembly:2049,$ChessMode:{$Column:19},$Easing:{$Left:$Jease$.$InCubic,$Top:$Jease$.$InCubic,$Zoom:$Jease$.$InCubic,$Opacity:$Jease$.$OutQuad,$Rotate:$Jease$.$InCubic},$Opacity:2,$Round:{$Rotate:0.8}}
+            ];
+            
+            var jssor_1_options = {
+              $AutoPlay: true,
+              $SlideshowOptions: {
+                $Class: $JssorSlideshowRunner$,
+                $Transitions: jssor_1_SlideshowTransitions,
+                $TransitionsOrder: 1
+              },
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $ThumbnailNavigatorOptions: {
+                $Class: $JssorThumbnailNavigator$,
+                $Rows: 2,
+                $Cols: 6,
+                $SpacingX: 14,
+                $SpacingY: 12,
+                $Orientation: 2,
+                $Align: 156
+              }
+            };
+            
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+            
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 960);
+                    refSize = Math.max(refSize, 300);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+            ScaleSlider();
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            //responsive code end
+        });
+    </script>
+	
