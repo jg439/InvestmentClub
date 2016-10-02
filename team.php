1aa7844230cd23139598d43c989fd9fe7e450603
Julia Garcia <jg439@njit.edu>
2016-10-02 19:41:53 -0400

    file

---
+++ team.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+	<html lang = "en">
+		<head>
+
+			<title>NJIT Investment Club </title>
+				<meta charset = "utf-8">
+				<meta name = "description" content="Student-managed club where you can find information about investing, our events and portfolio">
+				<meta name = "keywords" content ="NJIT, investing,investment,students, portfolio">
+
+				<link rel = "shortcut icon" href ="images/favicon.ico">
+				<link rel = "stylesheet" type ="text/css" href = "styles/main.css">
+		</head>
+
+		<body>
+			<header>
+				<nav id = "upper_nav">
+					<ul>
+						<li><a href = "#log">Login</a></li>
+						<li><a href = "registration.html">Register</a></li>
+						<li><a href = "">Search</a></li>
+					</ul>
+				</nav>
+
+
+				<img src = "images/NJIT_logo.png" alt = "logo" width ="250" height= "100">
+
+				<div id="header">
+					<ul class="nav">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="">About</a>
+							<ul>
+								<li><a href="team.php">Team</a></li>
+								<li><a href="challenges.php">Challenges</a></li>
+							</ul>
+						</li>
+							
+						<li><a href="">Portfolio</a>
+							<ul>
+								<li><a href="investmentphilosophy.php">Our Investment Philosophy</a></li>
+								<li><a href="executedtrades.php">Executed Trades</a></li>
+							</ul>
+						</li>
+							
+						<li><a href="calendar.php">Calendar</a></li>
+						<li><a href="">Classes</a>
+							<ul>
+								<li><a href="investing101.php">Investing 101</a></li>
+								<li><a href="bloomberg.php">Bloomberg Terminal</a></li>
+							</ul>
+						</li>
+						<li><a href="media.php">Media</a></li>
+						<li><a href="faq.php">FAQ</a></li>
+						<li><a href="contactus.php">Contact Us</a></li>
+			
+					</ul>
+				</div>
+			
+
+			</header>
+
+			<main>	
+				<h1> Executive Board </h1>
+					<div class="container">
+					
+					
+						<div class="blockimage"></div>
+						<img class ="blockimage" src = "images/Vlad-Shustov.jpg" width ="150" height = "250">
+						<div class="blockimage"><!-- Image--></div>
+						<div class="block" align="center"><h4 style = "color:#B71C1C">Julia Garcia</h4><p class = "smallbutton">President</p><!--Description--></div>
+						<div class="block" align="center"><h4 style = "color:#B71C1C">Vlad Shustov</h4><p class = "smallbutton">Executive VP</p>
+						Vlad is a senior majoring in business with a finance concentration. 
+						He was a forward for NJIT men's basketball team during the past three years.
+						Vlad spent last summer working as an assistant manager for MasterCorp, 
+						the leader in the American resort housekeeping industry. He also has been developing an
+						insurance-type start up that has recently started it's operations.
+						<br>
+						<img src = "images/gmail.png" width = "30" height = "30" style= "margin:3px">
+						</div>
+						<div class="block" align="center"><h4 style = "color:#B71C1C">Christian Foust</h4><p class = "smallbutton">Treasurer</p><!--Description--></div>
+						
+						<div class="blockimage"><!-- Image--></div>
+						<div class="blockimage"><!-- Image--></div>
+						<div class="blockimage"><!-- Image--></div>
+						<div class="block" align="center"><h4 style = "color:#B71C1C">Alana Dudley</h4><p class = "smallbutton">Secretary</p><!--Description--></div>
+						<div class="block" align="center"><h4 style = "color:#B71C1C">Ian Constantin</h4><p class = "smallbutton">Website Developer</p><!--Description--></div>
+						<div class="block" align="center"><h4 style = "color:#B71C1C">Michael Ehlrich</h4><p class = "smallbutton">Advisor</p><!--Description--></div>
+						
+				
+					</div>
+			</main>
+
+
+		</body>
+
+
+	</html>
