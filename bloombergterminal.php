96392507e17980816ea4ff70441ecd46826e130d
Julia Garcia <jg439@njit.edu>
2016-10-02 19:20:56 -0400

    bloomberg

---
+++ bloombergterminal.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+
+<title>NJIT Investment Club </title>
+<meta charset = "utf-8">
+<meta name = "description" content="Student-managed club where you can find information about investing, our events and portfolio">
+<meta name = "keywords" content ="NJIT, investing,investment,students, portfolio">
+
+<link rel = "shortcut icon" href ="images/favicon.ico">
+<link rel = "stylesheet" type ="text/css" href = "styles/main.css">
+</head>
+
+<body>
+<header>
+
+<nav id = "upper_nav">
+<ul>
+	<li><a href = "#log">Login</a></li>
+	<li><a href = "registration.html">Register</a></li>
+	<li><a href = "">Search</a></li>
+</ul>
+</nav>
+
+<img src = "images/NJIT_logo.png" alt = "logo" width ="300" height= "200">
+
+<div id="header">
+	<ul class="nav">
+		<li><a href="index.php">Home</a></li>
+		<li><a href="">About</a>
+			<ul>
+				<li><a href="team.php">Team</a></li>
+				<li><a href="challenges.php">Challenges</a></li>
+			</ul>
+		</li>
+			
+		<li><a href="">Portfolio</a>
+			<ul>
+				<li><a href="investmentphilosophy.php">Our Investment Philosophy</a></li>
+				<li><a href="executedtrades.php">Executed Trades</a></li>
+			</ul>
+		</li>
+			
+		<li><a href="calendar.php">Calendar</a></li>
+		<li><a href="">Classes</a>
+			<ul>
+				<li><a href="investing101.php">Investing 101</a></li>
+				<li><a href="bloomberg.php">Bloomberg Terminal</a></li>
+			</ul>
+		</li>
+		<li><a href="media.php">Media</a></li>
+		<li><a href="faq.php">FAQ</a></li>
+		<li><a href="contactus.php">Contact Us</a></li>
+
+	</ul>
+</div>
+
+</header>
+
+<main>	
+</main>
+
+
+</body>
+
+
+</html>
