ca3146fdec95b3607d0c31130297a542ab762ad7
Julia Garcia <jg439@njit.edu>
2016-10-02 18:55:51 -0400

    changed

---
+++ bloomberg.php
@@ -0,0 +1,105 @@
+CTYPE html>
+	<html lang = "en">
+		<head>
+
+			<title>NJIT Investment Club </title>
+			<meta charset = "utf-8">
+			<meta name = "description" content="Student-managed club where you can find information about investing, our events and portfolio">
+			<meta name = "keywords" content ="NJIT, investing,investment,students, portfolio">
+
+			<link rel = "shortcut icon" href ="images/favicon.ico">
+			<link rel = "stylesheet" type ="text/css" href = "styles/main.css">
+
+		</head>
+
+		<body>
+			<header>
+				<nav id = "upper_nav">
+					<ul>
+						<li><a href = "#log">Login</a></li>
+						<li><a href = "registration.html">Register</a></li>
+						<li><a href = "">Search</a></li>
+					</ul>
+				</nav>
+
+
+				<img src = "images/NJIT_logo.png" alt = "logo" width ="250" height= "100">
+
+				<div id="header">
+					<ul class="nav">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="">About</a>
+							<ul>
+								<li><a href="team.php">Team</a></li>
+								<li><a href="challenges.php">Challenges</a></li>
+							</ul>
+						</li>
+							
+						<li><a href="">Portfolio</a>
+							<ul>
+								<li><a href="investmentphilosophy.php">Our Investment Philosophy</a></li>
+								<li><a href="executedtrades.php">Executed Trades</a></li>
+							</ul>
+						</li>
+							
+						<li><a href="calendar.php">Calendar</a></li>
+						<li><a href="">Classes</a>
+							<ul>
+								<li><a href="investing101.php">Investing 101</a></li>
+								<li><a href="bloomberg.php">Bloomberg Terminal</a></li>
+							</ul>
+						</li>
+						<li><a href="media.php">Media</a></li>
+						<li><a href="faq.php">FAQ</a></li>
+						<li><a href="contactus.php">Contact Us</a></li>
+			
+					</ul>
+				</div>
+	
+			</header>
+       
+			<main>
+				<h1>Bloomberg Terminal</h1>
+	
+ 
+					<div class = "container2">
+						<h2> Bloomberg Essentials Online Training Program</h2>
+							<p>The Bloomberg Essentials program provides a convenient way to learn the basics of Bloomberg Professional financial information system. 
+							The BESS training consists of a series of Tutorial Documents,on-line Help screens and supporting documents. 
+							The tutorial materials are organized into 8 modules as follows:<p>
+
+								<ul>
+									<h3>Bloomberg Core</h3>
+										<li style= "list-style-type: disc" class ="limargin"><p class = "pmargin">Getting Started</p></li>
+										<li style= "list-style-type: disc" class ="limargin"><p class = "pmargin">Bloomberg News</p></li>
+										<li style= "list-style-type: disc" class ="limargin"><p class = "pmargin">Market Monitors and Launchpad</p></li>
+										<li style= "list-style-type: disc" class ="limargin"><p class = "pmargin">API (Bloomberg Data in Excel)</p></li>
+	
+								</ul>
+								
+								<ul>
+									<h3>Bloomberg Market Sector</h3>
+										<li style= "list-style: disc" class ="limargin"><p class = "pmargin">Equity Essentials</p></li>
+										<li style= "list-style: disc" class = "limargin"><p class = "pmargin">Fixed Income Essentials</p></li>
+										<li style= "list-style: disc" class = "limargin"><p class = "pmargin">FX (Foreign Exchange) Essentials</p></li>
+										<li style= "list-style: disc" class = "limargin"><p class = "pmargin">Commodity Essentials</p></li>
+								</ul>
+
+
+							<p>  The NJIT´s Bloomberg Terminal is located on the third floor of the Management Building. + Remember to create your
+							own account the first time that you log in. If you need more information about  how to create yor account, please
+							<a href= "http://www.np.edu.sg/library/resources/databases/Documents/Bloomberg%20BESS.pdf">click here.</a></p>
+	
+							<p>If you need help in the process of creating your login account, or if you have any questions about the BESS Program
+							please contact any member of the Investment Club by sending an email to: njitinvestmentclub@gmail.com</p>
+
+
+					</div>
+ 
+ 
+			</main>
+   
+		</body>
+	</html>
+
+
