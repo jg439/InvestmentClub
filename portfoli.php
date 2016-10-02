dc461afb9ac5f398789a91f78fa912dc197cc3a3
Julia Garcia <jg439@njit.edu>
2016-10-02 19:39:35 -0400

    file

---
+++ portfoli.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+
+<title>NJIT Investment Club </title>
+<meta charset = "utf-8">
+<meta name = "description" content="Student-managed club where you can find information about investing, our events and portfolio">
+<meta name = "keywords" content ="NJIT, investing,investment,students, portfolio">
+
+<link rel = "shortcut icon" href ="images/favicon.ico">
+<link rel = "stylesheet" type ="text/css" href = "styles/main.css">
+</head>
+
+<body>
+<header>
+
+<nav id = "upper_nav">
+<ul>
+	<li><a href = "#log">Login</a></li>
+	<li><a href = "registration.html">Register</a></li>
+	<li><a href = "">Search</a></li>
+</ul>
+</nav>
+
+<img src = "" alt = "logo" width ="150" height= "100">
+
+<h1>NJIT Investment Club</h1>
+
+<div id="header">
+	<ul class="nav">
+		<li><a href="index.php">Home</a></li>
+		<li><a href="">About</a>
+			<ul>
+				<li><a href="team.php">Team</a></li>
+				<li><a href="challenges.php">Challenges</a></li>
+			</ul>
+		</li>
+			
+		<li><a href="">Portfolio</a>
+			<ul>
+				<li><a href="investmentphilosophy.php">Our Investment Philosophy</a></li>
+				<li><a href="executedtrades.php">Executed Trades</a></li>
+			</ul>
+		</li>
+			
+		<li><a href="calendar.php">Calendar</a></li>
+		<li><a href="">Classes</a>
+			<ul>
+				<li><a href="investing101.php">Investing 101</a></li>
+				<li><a href="bloomberg.php">Bloomberg Terminal</a></li>
+			</ul>
+		</li>
+		<li><a href="media.php">Media</a></li>
+		<li><a href="faq.php">FAQ</a></li>
+		<li><a href="contactus.php">Contact Us</a></li>
+
+	</ul>
+</div>
+
+</header>
+
+<main>
+	<section>
+	<h1> Portfolio</h1>
+	<h2> Managing a real stock portfolio since the beginning of 2017</h2>
+	<p> The portfolio that the Investment Club is managing consists of different
+	companies that belong to technology and science related industries. At NJIT we believe 
+	that the only way of making good investment decisions is by knowing perfectly how 
+	businesses work. Therefore, we only invest in the following sectors:
+	<ul>
+	<li><h3>Healthcare</h3></li>
+	<li><h3>Technology</h3></li>
+	<li><h3>Industrials</h3></li>
+	<li><h3>Financial Services</h3></li>
+	<li><h3>Energy</h3></li>
+	<li><h3>Consumer</h3></li>
+	<li><h3>Communication Services</h3></li>
+	</ul>
+	</p>
+	
+	
+	
+	
+
+	</section>
+	
+	
+
+</main>
+</body>
+
+
+</html>
