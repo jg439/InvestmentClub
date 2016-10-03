4f1ed843cb50c4cf571906a45e380bba5fdc431a
Julia Garcia <jg439@njit.edu>
2016-10-02 21:05:52 -0400

    finished design of contact page

--- contactus.php
+++ contactus.php
@@ -45,8 +45,8 @@ if($_SESSION['sent'])
 
 				<nav id = "upper_nav">
 					<ul>
-						<li><a href = "#log">Login</a></li>
-						<li><a href = "registration.html">Register</a></li>
+						<li><a href = "index.php">Login</a></li>
+						<li><a href = "registration.php">Register</a></li>
 					</ul>
 				</nav>
 
@@ -89,23 +89,32 @@ if($_SESSION['sent'])
 
 			<main>	
 				<h1>Contact Us</h1>
-					<fieldset>
-						<p>Please direct us any questions or concerns</p>
-							<p>First Name</p>
-								<input type = "text" name= "firstname" value = "" >
-							<p>Last Name</p>
-								<input type = "text" name = "lastname" value = "">
-							<p>Comments</p>
-								<textarea name="comment" rows="5" cols="40"></textarea>
+				    
+				    <div class = "container3">
+						<center><h2>Please direct us any questions or concerns</h2></center>
+						<section style = "width:60%;">
+						<ul class = "contact">
+							
+							<li><h3>First Name </h3></li>                                         
+								 <input type = "text" name= "firstname" value = "" >
 						
+							<li><h3>Last Name</h3></li>
+						    <input type = "text" name = "lastname" value = "">
+							
+					       <li><h3> Email Address</h3></li>
+						   <input type = "email" name = "email" value = "">
+							
+							<h3>Comments</h3>
+							<textarea name="comment" rows="5" cols="40" style = "margin-left:43px;"></textarea>
+						    <br>
 							<input type = "Submit" name= "Submit" value= "Submit" class = "button">
-	 
-							<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1270.950082493455!2d-74.1775238345283!3d40.74341927822273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2ses!4v1465914604977" 
-							class= "googleimg"width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-	
-	
-					</fieldset>
-	
+					        </section>
+					         <aside style = "width:40%">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1960.065384419074!2d-74.17893032435067!3d40.74368236804558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x99358877b939dd16!2sMartin+Tuchman+School+of+Management!5e0!3m2!1sen!2sus!4v1475452587911"
+							width="400" height="350" frameborder="0" style="border:0;" allowfullscreen></iframe>
+	                         </aside>
+			
+	                <div>
 
 
 			</main>
