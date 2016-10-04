7df228854e57f44f6f73b80cf7acbaa85293ebac
Ian Constantin <iconstantin94@gmail.com>
2016-10-04 14:36:06 -0400

    moved navigation header to one php file

--- registration.php
+++ registration.php
@@ -12,50 +12,7 @@
 </head>
 
 <body>
-<header>
-
-
-<nav id = "upper_nav">
-<ul>
-	<li><a href = "index.php">Login</a></li>
-	<li><a href = "register.php">Register</a></li>
-</ul>
-</nav>
-<img src = "images/logo.svg" alt = "logo" width ="150" height= "100">
-
-<div id="header">
-	<ul class="nav">
-		<li><a href="index.php">Home</a></li>
-		<li><a href="">About</a>
-			<ul>
-				<li><a href="team.php">Team</a></li>
-				<li><a href="challenges.php">Challenges</a></li>
-			</ul>
-		</li>
-			
-		<li><a href="">Portfolio</a>
-			<ul>
-				<li><a href="investmentphilosophy.php">Our Investment Philosophy</a></li>
-				<li><a href="executedtrades.php">Executed Trades</a></li>
-			</ul>
-		</li>
-			
-		<li><a href="calendar.php">Calendar</a></li>
-		<li><a href="">Classes</a>
-			<ul>
-				<li><a href="investing101.php">Investing 101</a></li>
-				<li><a href="bloomberg.php">Bloomberg Terminal</a></li>
-			</ul>
-		</li>
-		<li><a href="media.php">Media</a></li>
-		<li><a href="faq.php">FAQ</a></li>
-		<li><a href="contactus.php">Contact Us</a></li>
-
-	</ul>
-</div>
-
-</header>
-
+<?php include 'navigation.php'?>
 
 <main>
 
