4e041fe790cebd971c0ebee1afba892084be985e
Ian Constantin <iconstantin94@gmail.com>
2016-10-04 15:41:26 -0400

    added email functionality

--- contactus.php
+++ contactus.php
@@ -49,22 +49,28 @@ if($_SESSION['sent'])
 				    <div class = "container3">
 						<center><h2>Please direct us any questions or concerns</h2></center>
 						<section style = "width:60%;">
-						<ul class = "contact">
-							
-							<li><h3>First Name </h3></li>                                         
-								 <input type = "text" name= "firstname" value = "" >
+							<ul class = "contact">
+							<form action="mail.php" id="mail" method="post">
+								<li><h3>First Name </h3></li> 
+								<input type="text" name="firstName" required>
+								
+								<li><h3>Last Name</h3></li>
+								<input type="text" name="lastName" required>
+								
+								<li><h3>Email Address</h3></li>
+								<input type="email" name="email" required>
+								
+								<h3>Comments</h3>
+								<textarea name="comment" form="mail" rows="5" cols="40" style = "margin-left:43px;"></textarea>
+								
+								
+								<br>
+								<input type="submit" value= "Send Email" class = "button">
+							</form>	
+							</ul>
+					    </section>
 						
-							<li><h3>Last Name</h3></li>
-						    <input type = "text" name = "lastname" value = "">
-							
-					       <li><h3> Email Address</h3></li>
-						   <input type = "email" name = "email" value = "">
-							
-							<h3>Comments</h3>
-							<textarea name="comment" rows="5" cols="40" style = "margin-left:43px;"></textarea>
-						    <br>
-							<input type = "Submit" name= "Submit" value= "Submit" class = "button">
-					        </section>
+						
 					         <aside style = "width:40%">
 							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1960.065384419074!2d-74.17893032435067!3d40.74368236804558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x99358877b939dd16!2sMartin+Tuchman+School+of+Management!5e0!3m2!1sen!2sus!4v1475452587911"
 							width="400" height="350" frameborder="0" style="border:0;" allowfullscreen></iframe>
---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php
+	
+	$to = "ic36@njit.edu";
+	$subject = "From - ".$_POST['firstName']." ".$_POST['lastName']." (".$_POST['email'].")";
+	$message = $_POST['comment'];
+	
+	mail($to,$subject,$message);
+	
+	header("Location: contactus.php");
+?>
